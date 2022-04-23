880d548dfebd2231204c17fa63e3226d24d3738e
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-04-23 12:14:35 -0300

    primeiro select funcionou

--- authapi_supabase/config/dbPostgress.js
+++ authapi_supabase/config/dbPostgress.js
@@ -1,9 +1,11 @@
-import {pool} from 'pg'
+import {Pool} from 'pg'
 require('dotenv/config');
 
+console.log(process.env.DB_USER)
 
 //Configurar as variaveis de ambiente na vercel e utilizar os secrets 
 const pool = new Pool({
+    
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
--- authapi_supabase/pages/api/supabase.ts
+++ authapi_supabase/pages/api/supabase.ts
@@ -1,27 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import {createClient} from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://tmdchumipyjkmoihteyt.supabase.co"
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtZGNodW1pcHlqa21vaWh0ZXl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA3MTU5NDcsImV4cCI6MTk2NjI5MTk0N30.iUhzFMOtp9NZW5MoXWZjvWabMn8E6Q336U6DXXsEnt0"
 
-const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY)
+//const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY)
+import pool from '../../config/dbPostgress'
 
+export async function handler(req, res) {
 
+  try {
+    pool.query('SELECT * FROM USERS', (e, result) => {
+      
+      //retorna objeto com resultado da query
+      //result.rows[0]
+      console.log(result.rows[0])
+    })
 
-export async function handler (req, res) {
+    //console.log(res)
+    res.status(200).json({ res })
 
-  const { data, error } = await supabase
-  .from('users')
-  .select()
-  try{
-    console.log(data)
-    res.status(200).json({ data })
-
-  }catch (erro){
-    res.status(200).json({ error })
+  } catch (e) {
+    res.status(400).json({ e })
   }
-  
+
 }
 
 
